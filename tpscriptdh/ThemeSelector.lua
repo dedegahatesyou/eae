0d8c5f7b1e7e67531bb77c195c44a8b194baa7f5
dedegahatesyou <96036855+dedegahatesyou@users.noreply.github.com>
2022-10-17 13:04:52 -0300

    Update ThemeSelector.lua

--- tpscriptdh/ThemeSelector.lua
+++ tpscriptdh/ThemeSelector.lua
@@ -1,3 +1,162 @@
+local teleports = game.CoreGui:WaitForChild("teleports")
+
+
+
+
+local	SettingsTab = Instance.new("Frame")
+local	UICorner_36 = Instance.new("UICorner")
+local	SettingsWindow = Instance.new("Frame")
+local	UICorner_37 = Instance.new("UICorner")
+local	Buttons_4 = Instance.new("Folder")
+local	ImageButton_2 = Instance.new("ImageButton")
+local	DropDown = Instance.new("Frame")
+local	Selection = Instance.new("TextButton")
+local	Menu = Instance.new("ScrollingFrame")
+local	UI = Instance.new("UIListLayout")
+local	green = Instance.new("TextButton")
+local	blue = Instance.new("TextButton")
+local	red = Instance.new("TextButton")
+local	purp = Instance.new("TextButton")
+local	ThemeValues = Instance.new("Folder")
+local	UIGradient_50 = Instance.new("UIGradient")
+local	shadow = Instance.new("Folder")
+local	UmbraShadow = Instance.new("ImageLabel")
+local	GradColor = Instance.new("UIGradient")
+
+local redtoggle = Instance.new("BoolValue")
+local bluetoggle = Instance.new("BoolValue")
+local greentoggle = Instance.new("BoolValue")
+local purpletoggle = Instance.new("BoolValue")
+
+SettingsTab.Name = "SettingsTab"
+SettingsTab.Parent = teleports.Tabs
+SettingsTab.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+SettingsTab.Position = UDim2.new(0.00864717271, 0, 0.0213459134, 0)
+SettingsTab.Size = UDim2.new(0, 586, 0, 253)
+SettingsTab.Visible = false
+
+UICorner_36.CornerRadius = UDim.new(0, 15)
+UICorner_36.Parent = SettingsTab
+
+SettingsWindow.Name = "SettingsWindow"
+SettingsWindow.Parent = SettingsTab
+SettingsWindow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SettingsWindow.BorderSizePixel = 0
+SettingsWindow.Position = UDim2.new(0.0145719489, 0, 0.0367540196, 0)
+SettingsWindow.Size = UDim2.new(0, 567, 0, 237)
+SettingsWindow.Visible = false
+
+UICorner_37.CornerRadius = UDim.new(0, 5)
+UICorner_37.Parent = SettingsWindow
+
+Buttons_4.Name = "Buttons"
+Buttons_4.Parent = SettingsWindow
+
+ImageButton_2.Parent = Buttons_4
+ImageButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton_2.BackgroundTransparency = 1.000
+ImageButton_2.Position = UDim2.new(0.939849555, 0, 0.860759497, 0)
+ImageButton_2.Size = UDim2.new(0, 25, 0, 25)
+ImageButton_2.Image = "rbxassetid://11278495791"
+ImageButton_2.ImageColor3 = Color3.fromRGB(0, 255, 0)
+
+DropDown.Name = "DropDown"
+DropDown.Parent = Buttons_4
+DropDown.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DropDown.BackgroundTransparency = 1.000
+DropDown.BorderSizePixel = 0
+DropDown.Position = UDim2.new(0.039903, 0, 0.0523207784, 0)
+DropDown.Size = UDim2.new(0.360125929, 0, 0.155168667, 0)
+
+Selection.Name = "Selection"
+Selection.Parent = DropDown
+Selection.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Selection.BackgroundTransparency = 1.000
+Selection.Size = UDim2.new(1.00274909, 0, 1, 0)
+Selection.Font = Enum.Font.SourceSansSemibold
+Selection.Text = "Select Theme"
+Selection.TextColor3 = Color3.fromRGB(255, 255, 255)
+Selection.TextScaled = true
+Selection.TextSize = 14.000
+Selection.TextWrapped = true
+
+Menu.Name = "Menu"
+MenuParent = DropDown
+Menu.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Menu.BackgroundTransparency = 1.000
+Menu.BorderSizePixel = 0
+Menu.Position = UDim2.new(0, 0, 1, 0)
+Menu.Size = UDim2.new(1, 0, 0, 0)
+Menu.ScrollBarThickness = 8
+
+UI.Name = "UI"
+UI.Parent = Menu
+UI.SortOrder = Enum.SortOrder.LayoutOrder
+
+green.Name = "green"
+green.Parent = Menu
+green.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+green.BackgroundTransparency = 1.000
+green.BorderSizePixel = 0
+green.Size = UDim2.new(1, 0, 0, 35)
+green.Visible = false
+green.Font = Enum.Font.SourceSans
+green.Text = "Green"
+green.TextColor3 = Color3.fromRGB(255, 255, 255)
+green.TextScaled = true
+green.TextSize = 20.000
+green.TextWrapped = true
+
+blue.Name = "blue"
+blue.Parent = Menu
+blue.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+blue.BackgroundTransparency = 1.000
+blue.BorderSizePixel = 0
+blue.Size = UDim2.new(1, 0, 0, 35)
+blue.Visible = false
+blue.Font = Enum.Font.SourceSans
+blue.Text = "Blue"
+blue.TextColor3 = Color3.fromRGB(255, 255, 255)
+blue.TextScaled = true
+blue.TextSize = 20.000
+blue.TextWrapped = true
+
+red.Name = "red"
+red.Parent = Menu
+red.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+red.BackgroundTransparency = 1.000
+red.BorderSizePixel = 0
+red.Size = UDim2.new(1, 0, 0, 35)
+red.Visible = false
+red.Font = Enum.Font.SourceSans
+red.Text = "Red"
+red.TextColor3 = Color3.fromRGB(255, 255, 255)
+red.TextScaled = true
+red.TextSize = 20.000
+red.TextWrapped = true
+
+purp.Name = "purp"
+purp.Parent = Menu
+purp.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+purp.BackgroundTransparency = 1.000
+purp.BorderSizePixel = 0
+purp.Size = UDim2.new(1, 0, 0, 35)
+purp.Visible = false
+purp.Font = Enum.Font.SourceSans
+purp.Text = "Purple"
+purp.TextColor3 = Color3.fromRGB(255, 255, 255)
+purp.TextScaled = true
+purp.TextSize = 20.000
+purp.TextWrapped = true
+
+ThemeValues.Name = "ThemeValues"
+ThemeValues.Parent = DropDown
+
+UIGradient_50.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(80, 80, 80)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(3, 2, 2))}
+UIGradient_50.Rotation = 90
+UIGradient_50.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.50), NumberSequenceKeypoint.new(1.00, 0.50)}
+UIGradient_50.Parent = SettingsWindow
+
 local drop = script.Parent
 local menu = drop.Menu
 local open = menu.Open.Value
@@ -38,7 +197,6 @@ UIGradient_7
 UIGradient_8
 UIGradient_9
 UIGradient_10
-UIGradient_11
 UIGradient_12
 UIGradient_13
 UIGradient_14
@@ -59,7 +217,6 @@ UIGradient_28
 UIGradient_29
 UIGradient_30
 UIGradient_31
-UIGradient_32
 UIGradient_33
 UIGradient_34
 UIGradient_35
@@ -134,12 +291,6 @@ while true do
 	
 	if ThemeValues.redtheme.Value == true then
 		
-		UIGradient.Color = ColorSequence.new{
-			
-			ColorSequenceKeypoint.new(0, red),
-			ColorSequenceKeypoint.new(1, Dred)
-			
-		}
 		UIGradient_2.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, red),
@@ -189,12 +340,6 @@ while true do
 
 		}
 		UIGradient_10.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, red),
-			ColorSequenceKeypoint.new(1, Dred)
-
-		}
-		UIGradient_11.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, red),
 			ColorSequenceKeypoint.new(1, Dred)
@@ -320,12 +465,6 @@ while true do
 			ColorSequenceKeypoint.new(1, Dred)
 
 		}
-		UIGradient_32.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, red),
-			ColorSequenceKeypoint.new(1, Dred)
-
-		}
 		UIGradient_33.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, red),
@@ -431,13 +570,6 @@ while true do
 		
 	elseif ThemeValues.bluetheme.Value == true then	
 	
-
-			UIGradient.Color = ColorSequence.new{
-
-				ColorSequenceKeypoint.new(0, blue),
-				ColorSequenceKeypoint.new(1, Dblue)
-
-			}
 			UIGradient_2.Color = ColorSequence.new{
 
 				ColorSequenceKeypoint.new(0, blue),
@@ -487,12 +619,6 @@ while true do
 
 			}
 			UIGradient_10.Color = ColorSequence.new{
-
-				ColorSequenceKeypoint.new(0, blue),
-				ColorSequenceKeypoint.new(1, Dblue)
-
-			}
-			UIGradient_11.Color = ColorSequence.new{
 
 				ColorSequenceKeypoint.new(0, blue),
 				ColorSequenceKeypoint.new(1, Dblue)
@@ -618,12 +744,6 @@ while true do
 				ColorSequenceKeypoint.new(1, Dblue)
 
 			}
-			UIGradient_32.Color = ColorSequence.new{
-
-				ColorSequenceKeypoint.new(0, blue),
-				ColorSequenceKeypoint.new(1, Dblue)
-
-			}
 			UIGradient_33.Color = ColorSequence.new{
 
 				ColorSequenceKeypoint.new(0, blue),
@@ -730,12 +850,6 @@ while true do
 	elseif ThemeValues.greentheme.Value == true then	
 
 
-		UIGradient.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, green),
-			ColorSequenceKeypoint.new(1, Ogreen)
-
-		}
 		UIGradient_2.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, green),
@@ -785,12 +899,6 @@ while true do
 
 		}
 		UIGradient_10.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, green),
-			ColorSequenceKeypoint.new(1, Ogreen)
-
-		}
-		UIGradient_11.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, green),
 			ColorSequenceKeypoint.new(1, Ogreen)
@@ -911,12 +1019,6 @@ while true do
 
 		}
 		UIGradient_31.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, green),
-			ColorSequenceKeypoint.new(1, Ogreen)
-
-		}
-		UIGradient_32.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, green),
 			ColorSequenceKeypoint.new(1, Ogreen)
@@ -1027,13 +1129,6 @@ while true do
 		
 	elseif ThemeValues.purpletheme.Value == true then	
 
-
-		UIGradient.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, purple),
-			ColorSequenceKeypoint.new(1, Dpurple)
-
-		}
 		UIGradient_2.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, purple),
@@ -1083,12 +1178,6 @@ while true do
 
 		}
 		UIGradient_10.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, purple),
-			ColorSequenceKeypoint.new(1, Dpurple)
-
-		}
-		UIGradient_11.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, purple),
 			ColorSequenceKeypoint.new(1, Dpurple)
@@ -1209,12 +1298,6 @@ while true do
 
 		}
 		UIGradient_31.Color = ColorSequence.new{
-
-			ColorSequenceKeypoint.new(0, purple),
-			ColorSequenceKeypoint.new(1, Dpurple)
-
-		}
-		UIGradient_32.Color = ColorSequence.new{
 
 			ColorSequenceKeypoint.new(0, purple),
 			ColorSequenceKeypoint.new(1, Dpurple)
